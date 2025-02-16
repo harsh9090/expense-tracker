b438cb396b0e5a015701998255d61b454d4b62d6
Harsh <harsh.mithaiwala.hm@gmail.com>
2025-02-15 23:52:22 -0500

    updates

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://expense-tracker-api.onrender.com' // Backend URL
+    apiUrl: 'https://expense-tracker-backend-kq9k.onrender.com' // Backend URL
 };
